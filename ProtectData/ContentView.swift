867778522e818e1711ecbf24ea33d689a72b2e66
aalmanms <aalmanms@sdaia.gov.sa>
2024-09-07 21:00:20 +0300

    Initial Commit

---
+++ ProtectData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProtectData
+//
+//  Created by Dev.SHooooooQ on 04/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProtectData/ProtectDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtectDataApp.swift
+//  ProtectData
+//
+//  Created by Dev.SHooooooQ on 04/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ProtectDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
